c1d57a02be3968f19ba33b7f93ac3ea76f21bed9
Aarthy JR <aarthyjogi26@gmail.com>
2025-03-24 15:07:13 +0530

    updated

--- app.py
+++ app.py
@@ -572,7 +572,7 @@ from firebase_admin import credentials, firestore
 
 # 🔹 Initialize Firebase
 if not firebase_admin._apps:
-    cred = credentials.Certificate("D:\\Glove\\newkey.json")
+    cred = credentials.Certificate("D:\Glove\glove\newkey.json")
 
     firebase_admin.initialize_app(cred)
 
